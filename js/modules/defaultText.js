8faeb34952bc2d1f16a30871e58159b87b625bc3
dannesx <daniel.viantunes@gmail.com>
2023-01-26 21:21:44 -0300

    Adicionando Rodrigo + Code, CSS e HTML

--- js/modules/defaultText.js
+++ js/modules/defaultText.js
@@ -1,16 +1,13 @@
 export default function defaultText({
 	professor,
 	ferramenta,
-	turma,
 	temaAula,
 	aula,
-	projeto,
 	desafio,
 	complementar,
 	temConteudo,
 }) {
 	return `📆 Data: ${getNowDate()}
-😃 Turma: ${turma}
 📚 Tema Aula: ${temaAula}
 🧰 Ferramenta: ${ferramenta.nome}
 
@@ -19,7 +16,7 @@ ${
 }💬 Olá pais, mães e responsáveis! É ${
 		professor.pronome === 'ele/dele' ? 'o professor' : 'a professora'
 	} *${professor.nome}* passando o feedback da aula! 🪄
-${temConteudo[0] ? `\n*O que fizemos hoje em aula?*\n${aula}\n` : ''}${temConteudo[1] ? `\n*Sobre o projeto*\n${projeto}\n` : ''}
+${temConteudo[0] ? `\n🎯 *O que fizemos hoje em aula?*\n${aula}\n` : ''}${temConteudo[1] ? `\n${professor.pronome === 'ele/dele' ? '👨‍💻' : '👩‍💻'} *Qual Desafio para a próxima aula?*\n${desafio}\n` : ''}${temConteudo[2] ? `\n🤩 *Prática complementar recomendada para semana*\n${complementar}\n` : ''}
 *Qual ferramenta usamos e como acessar/instalar?*
 Utilizamos ${ferramenta.nome}, é ${
 		ferramenta.download
@@ -27,7 +24,7 @@ Utilizamos ${ferramenta.nome}, é ${
 			: 'acessado no navegador (ex: Google Chrome)'
 	}.
 Link: ${ferramenta.links.link}
-${temConteudo[2] ? `\n${professor.pronome === 'ele/dele' ? '👨‍💻' : '👩‍💻'} *Qual Desafio para a próxima aula?*\n${desafio}\n` : ''} ${temConteudo[3] ? `\n*Prática complementar recomendada para semana*\n${complementar}\n` : ''}
+
 Em caso de dúvidas, chama a gente.
 Desejamos bons estudos e uma ótima semana!
 `
