2cdbee8eb2952a9691816f129e7fe43c2aee6195
dannesx <daniel.viantunes@gmail.com>
2023-08-21 15:35:44 -0300

    Melhorando o Texto Padrão

--- js/modules/defaultText.js
+++ js/modules/defaultText.js
@@ -7,17 +7,16 @@ export default function defaultText({
 	complementar,
 	temConteudo,
 }) {
-	return `📚 Tema Aula: ${temaAula}
-🧰 Ferramenta: ${ferramenta.nome}
-
-${
+	return `${
 	professor.pronome === 'ele/dele' ? '👨‍🏫' : '👩‍🏫'
 }💬 Olá pais, mães e responsáveis! É ${
-		professor.pronome === 'ele/dele' ? 'o professor' : 'a professora'
-	} *${professor.nome}* passando o feedback da aula! 🪄
-${temConteudo[0] ? `\n🎯 *O que fizemos hoje em aula?*\n${aula}\n` : ''}${temConteudo[1] ? `\n${professor.pronome === 'ele/dele' ? '👨‍💻' : '👩‍💻'} *Qual Desafio para a próxima aula?*\n${desafio}\n` : ''}${temConteudo[2] ? `\n🤩 *Prática complementar recomendada para semana*\n${complementar}\n` : ''}
-*Qual ferramenta usamos e como acessar/instalar?*
-Utilizamos ${ferramenta.nome}, é ${
+		professor.pronome === 'ele/dele' ? 'o prof.' : 'a prof.'
+	} *${professor.nome}* passando o feedback da aula!
+	
+📚 *A última aula foi sobre _${temaAula}_* 
+${temConteudo[0] ? `${aula}\n` : ''}${temConteudo[1] ? `\n${professor.pronome === 'ele/dele' ? '👨‍💻' : '👩‍💻'} *Qual desafio para a próxima aula?*\n${desafio}\n` : ''}${temConteudo[2] ? `\n🤩 *Prática complementar recomendada para semana*\n${complementar}\n` : ''}
+🧰 *Qual ferramenta usamos e como acessar/instalar?*
+Utilizamos *${ferramenta.nome}*, é ${
 		ferramenta.download
 			? 'instalado no computador'
 			: 'acessado no navegador (ex: Google Chrome)'
@@ -25,6 +24,6 @@ Utilizamos ${ferramenta.nome}, é ${
 Link: ${ferramenta.links.link}
 
 Em caso de dúvidas, chama a gente.
-Desejamos bons estudos e uma ótima semana!
+Desejamos bons estudos e uma ótima semana! ✨🚀
 `
 }
